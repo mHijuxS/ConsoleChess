1be5d2c6cafae8caf17a451deec6a45baf86b6a4
mHijuxS <matheus_sf@outlook.com.br>
2023-06-22 17:02:39 -0300

    Implementing the pawn movement

--- Chess/game/Pawn.cs
+++ Chess/game/Pawn.cs
@@ -10,29 +10,75 @@ namespace game
             Piece p = Board.piece(pos);
             return p == null || p.Color != Color;
         }
+
+        private bool Free(Position position)
+        {
+            return Board.piece(position) == null;
+        }
+
+        private bool EnemyExists(Position position)
+        {
+            Piece p = Board.piece(position);
+            return p != null && p.Color != Color;
+        }
         public override bool[,] PossibleMoves()
         {
             Position pos = new Position(0, 0);
             bool[,] mat = new bool[Board.Rows, Board.Columns];
 
-            if (Color == Color.Black)
+            if(Color == Color.White)
             {
-                //abaixo
-                pos.DefineValues(Position.Row + 1, Position.Column);
-                if (Board.ValidPosition(pos) && CanMove(pos))
+                pos.DefineValues(Position.Row - 1, Position.Column);
+                if (Board.ValidPosition(pos) && Free(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row - 2, Position.Column);
+                if (Board.ValidPosition(pos) && Free(pos) && MoveCounter==0)
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row -1, Position.Column -1);
+                if (Board.ValidPosition(pos)  && EnemyExists(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row - 1, Position.Column + 1);
+                if (Board.ValidPosition(pos) && EnemyExists(pos))
                 {
                     mat[pos.Row, pos.Column] = true;
                 }
             }
             else
             {
-                //acima
-                pos.DefineValues(Position.Row - 1, Position.Column);
-                if (Board.ValidPosition(pos) && CanMove(pos))
+                pos.DefineValues(Position.Row + 1, Position.Column);
+                if (Board.ValidPosition(pos) && Free(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row + 2, Position.Column);
+                if (Board.ValidPosition(pos) && Free(pos) && MoveCounter == 0)
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row + 1, Position.Column - 1);
+                if (Board.ValidPosition(pos) && EnemyExists(pos))
+                {
+                    mat[pos.Row, pos.Column] = true;
+                }
+
+                pos.DefineValues(Position.Row + 1, Position.Column + 1);
+                if (Board.ValidPosition(pos) && EnemyExists(pos))
                 {
                     mat[pos.Row, pos.Column] = true;
                 }
             }
+
             return mat;
         }
         public override string ToString()
